ac8ad57b6b55888eb29938114127b3392a91df22
daryal89 <creativity80@gmail.com>
2018-12-01 22:41:35 -0500

    added some rando code

--- src/main/java/javapractice/DhrubaClass.java
+++ src/main/java/javapractice/DhrubaClass.java
@@ -1,12 +1,14 @@
 package javapractice;
 
 public class DhrubaClass {
-	int a=10;
-	int b=20;
-	int c=30;
-	int d=40;
+	int c = 30;
+	int d = 40;
+	String str = "hari";
+	String s1 = "mohan";
+
+	boolean b = false;
+	boolean e = true;
 	
-	String str ="hari";
-	String s1 ="mohan";
-	
+	// hello this is git add method
+
 }
